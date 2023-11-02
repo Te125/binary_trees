bfa50aabafc92987f724c27a9d1d9d6a57cf7251
Uchenna Oko <uche4newton1981@gmail.com>
2023-11-02 14:49:52 +0100

    Measures the height of a binary tree-fixed-omitted-items-added

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,7 +16,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	l_height = binary_tree_height(tree->left);
-	r_height = binary_tree_height(tree->right);
-	return (1 + (l_height > r_height ? l_height : r_height));
+	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (l_height > r_height ? l_height : r_height);
 }
