9202574498bbf82d2e724aac4cb55c3d45990888
Uchenna Oko <uche4newton1981@gmail.com>
2023-11-02 13:18:11 +0100

    Updating Header File

--- binary_trees.h
+++ binary_trees.h
@@ -30,10 +30,10 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-int main(void);
 void binary_tree_print(const binary_tree_t *tree);
 /*static size_t _height(const binary_tree_t *tree);*/
 /*static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);*/
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -53,5 +53,9 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+/*** Helper Functions ***/
+size_t _treeheight(const binary_tree_t *tree);
+size_t _getheight(const binary_tree_t *root);
 
 #endif
