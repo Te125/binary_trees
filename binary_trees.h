99c1692f46359671a26b3160c442718919001b66
Uchenna Oko <uche4newton1981@gmail.com>
2023-11-04 01:43:05 +0100

    Updating Header File

--- binary_trees.h
+++ binary_trees.h
@@ -53,9 +53,14 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 /*** Helper Functions ***/
 size_t _treeheight(const binary_tree_t *tree);
 size_t _getheight(const binary_tree_t *root);
+void _traverselevel(const binary_tree_t *tree, int level, void (*func)(int));
+size_t binary_tree_depth(const binary_tree_t *tree);
 
 #endif
